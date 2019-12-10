34604358880d7e15ebd9d88730aa0d5318151477
Mark A Guza <guzamaguza@gmail.com>
2019-12-10 05:53:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,13 @@
 class Anagram
   attr_accessor :word
 
-  def initialize(word) 
+  def initialize(word)
     @word = word
   end
 
   def match(word_array)
     word_array.select do |element|
-      (@word.split("")).sort = (element.split("")).sort 
+      (@word.split("")).sort = (element.split("")).sort
     end
   end
 
